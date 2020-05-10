2d3c97a49839e5b9b685921c2a45d5d84969aa62
Alfredo Martinez <amartinez0512@isnstudio.com>
2020-05-10 00:34:33 -0600

    migraciones basicas

--- main/models.py
+++ main/models.py
@@ -4,15 +4,19 @@ from django.db import models
 # Create your models here.
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 
 class CommonModel(models.Model):
-    created_on = models.DateTimeField(auto_now_add=True, auto_now=False)
-    updated_on = models.DateTimeField(auto_now=True, auto_now_add=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
 
 
 class Profile(CommonModel):
-    photo = models.FileField(upload_to='profile_images/')
+    photo = models.FileField(upload_to='profile_images/', null=True, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
 
@@ -60,19 +64,19 @@ class CharacterEquipment(CommonModel):
                                   null=True, blank=True)
     right_hand = models.ForeignKey("cat.Weapon", on_delete=models.CASCADE, related_name="eqpmt_right_hand",
                                    null=True, blank=True)
-    ring = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    ring = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_ring",
                              null=True, blank=True)
-    brace = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    brace = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_brace",
                               null=True, blank=True)
-    head = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    head = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_head",
                              null=True, blank=True)
-    chest = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    chest = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_chest",
                               null=True, blank=True)
-    bottom = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    bottom = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_bottom",
                                null=True, blank=True)
-    belt = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    belt = models.ForeignKey("cat.Accessory", on_delete=models.CASCADE, related_name="eqpmt_belt",
                              null=True, blank=True)
-    foot = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_left_hand",
+    foot = models.ForeignKey("cat.Armor", on_delete=models.CASCADE, related_name="eqpmt_foot",
                              null=True, blank=True)
 
 
